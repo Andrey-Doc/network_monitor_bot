0fac5da50773ef39d70664a5a21f972696773e29
Andrey-Doc <mandrew1986@gmail.com>
2025-06-23 16:39:13 +0200

    another update

--- telegram_bot/bot/main.py
+++ telegram_bot/bot/main.py
@@ -125,51 +125,51 @@ async def send_welcome(message: Message):
         reply_markup=main_menu_keyboard(lang=get_lang())
     )
 
-@dp.message_handler(lambda m: m.text == 'Настройки')
+@dp.message_handler(lambda m: m.text == translate(get_lang(), 'settings_menu'))
 async def handle_settings_menu(message: Message):
     await message.answer(translate(get_lang(), 'settings_menu'), reply_markup=settings_menu_keyboard(lang=get_lang()))
 
-@dp.message_handler(lambda m: m.text == 'Мониторинг')
+@dp.message_handler(lambda m: m.text == translate(get_lang(), 'monitoring_menu'))
 async def handle_monitoring_menu(message: Message):
     await message.answer(translate(get_lang(), 'monitoring_menu'), reply_markup=monitoring_menu_keyboard(lang=get_lang()))
 
-@dp.message_handler(lambda m: m.text == 'Сканирование')
+@dp.message_handler(lambda m: m.text == translate(get_lang(), 'scan_menu'))
 async def handle_scan_menu(message: Message):
     await message.answer(translate(get_lang(), 'scan_menu'), reply_markup=scan_menu_keyboard(lang=get_lang()))
 
-@dp.message_handler(lambda m: m.text == 'Уведомления')
+@dp.message_handler(lambda m: m.text == translate(get_lang(), 'notification_menu'))
 async def handle_notification_menu(message: Message):
-    await message.answer(translate(get_lang(), 'notifications_menu'), reply_markup=notification_menu_keyboard(lang=get_lang()))
+    await message.answer(translate(get_lang(), 'notification_menu'), reply_markup=notification_menu_keyboard(lang=get_lang()))
 
-@dp.message_handler(lambda m: m.text == 'Роутеры')
+@dp.message_handler(lambda m: m.text == translate(get_lang(), 'router_menu'))
 async def handle_router_menu(message: Message):
-    await message.answer(translate(get_lang(), 'routers_menu'), reply_markup=router_menu_keyboard(lang=get_lang()))
+    await message.answer(translate(get_lang(), 'router_menu'), reply_markup=router_menu_keyboard(lang=get_lang()))
 
-@dp.message_handler(lambda m: m.text == 'Интерфейс')
+@dp.message_handler(lambda m: m.text == translate(get_lang(), 'interface_menu'))
 async def handle_interface_menu(message: Message):
     await message.answer(translate(get_lang(), 'interface_menu'), reply_markup=interface_menu_keyboard(lang=get_lang()))
 
-@dp.message_handler(lambda m: m.text == 'Безопасность')
+@dp.message_handler(lambda m: m.text == translate(get_lang(), 'security_menu'))
 async def handle_security_menu(message: Message):
     await message.answer(translate(get_lang(), 'security_menu'), reply_markup=security_menu_keyboard(lang=get_lang()))
 
-@dp.message_handler(lambda m: m.text == 'Резервное копирование')
+@dp.message_handler(lambda m: m.text == translate(get_lang(), 'backup_menu'))
 async def handle_backup_menu(message: Message):
     await message.answer(translate(get_lang(), 'backup_menu'), reply_markup=backup_menu_keyboard(lang=get_lang()))
 
-@dp.message_handler(lambda m: m.text == 'Экспорт/Импорт')
+@dp.message_handler(lambda m: m.text == translate(get_lang(), 'export_menu'))
 async def handle_export_menu(message: Message):
     await message.answer(translate(get_lang(), 'export_menu'), reply_markup=export_menu_keyboard(lang=get_lang()))
 
-@dp.message_handler(lambda m: m.text == 'Помощь')
+@dp.message_handler(lambda m: m.text == translate(get_lang(), 'help_menu'))
 async def handle_help_menu(message: Message):
     await message.answer(translate(get_lang(), 'help_menu'), reply_markup=help_menu_keyboard(lang=get_lang()))
 
-@dp.message_handler(lambda m: m.text == 'Назад в меню')
+@dp.message_handler(lambda m: m.text == translate(get_lang(), 'back_to_main'))
 async def handle_back_to_main(message: Message):
     await message.answer(translate(get_lang(), 'back_to_main'), reply_markup=main_menu_keyboard(lang=get_lang()))
 
-@dp.message_handler(lambda m: m.text == 'Назад к настройкам')
+@dp.message_handler(lambda m: m.text == translate(get_lang(), 'back_to_settings'))
 async def handle_back_to_settings(message: Message):
     await message.answer(translate(get_lang(), 'back_to_settings'), reply_markup=settings_menu_keyboard(lang=get_lang()))
 
@@ -799,11 +799,11 @@ async def handle_scan_ttl(message: Message):
 async def process_scan_ttl(message: Message, state: FSMContext):
     try:
         ttl = int(message.text.strip())
-        if ttl < 60 or ttl > 86400:
+        if ttl < 60 or ttl > 604800:  # 7 дней
             await message.answer(translate(get_lang(), 'scan_ttl_error'))
             return
         if settings_manager.set_setting('scanning.results_ttl', ttl):
-            await message.answer(translate(get_lang(), 'scan_ttl_set', ttl=ttl))
+            await message.answer(translate(get_lang(), 'scan_ttl_set', value=ttl))
         else:
             await message.answer(translate(get_lang(), 'scan_ttl_save_error'))
     except Exception:
